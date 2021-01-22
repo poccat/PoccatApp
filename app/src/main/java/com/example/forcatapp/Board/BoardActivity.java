3dab23d62e99a472cd2a719b99b0d1451e347555
harim100 <alfeka07@gmail.com>
2021-01-22 20:34:12 +0900

    공통, 후원게시판 연동(웹뷰), 부트페이 모바일 적용(공기계로 테스트 필요)

--- app/src/main/java/com/example/forcatapp/Board/BoardActivity.java
+++ app/src/main/java/com/example/forcatapp/Board/BoardActivity.java
@@ -1,25 +1,98 @@
 package com.example.forcatapp.Board;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.forcatapp.Chat.ChatActivity;
+import com.example.forcatapp.Group.GroupActivity;
+import com.example.forcatapp.Main.MainActivity;
+import com.example.forcatapp.Map.LocationActivity;
 import com.example.forcatapp.R;
 import com.example.forcatapp.util.BottomNavBarHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class BoardActivity extends AppCompatActivity {
+    private static final String TAG = "BoardActivity";
     private static final int ACTIVITY_NUMBER = 1;
     private Context mContext = BoardActivity.this;
+    static final String[] LIST_MENU = {"공지사항", "후원게시판", "나눔게시판", "입양후기", "봉사게시판"} ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_board);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) findViewById(R.id.lv_board) ;
+        listview.setAdapter(adapter) ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get TextView's Text.
+                String strText = (String) parent.getItemAtPosition(position);
+
+                // 리스트뷰에서 선택한 게시판을 받아온다
+                // 웹뷰 프래그먼트로 넘어가 해당 게시판페이지를 보여준다
+                // 그러기위해서 switch-case로 웹뷰 주소를 설정한 후 넘긴다
+                String url = "";
+                int caseNum = 0;
+                switch (position){
+                    case 0:  //공지사항
+                        url = "http://192.168.0.51:9005/secondB/common_board_list.foc?com_b_type=0";
+                        break;
+                    case 1: //후원게시판
+                        url = "http://192.168.0.51:9005/secondB/donation_noti_list.foc";
+                        caseNum = 1;
+                        break;
+                    case 2:  //나눔게시판
+                        url = "http://192.168.0.51:9005/secondB/common_board_list.foc?com_b_type=3";
+                        break;
+                    case 3:  //입양후기
+                        url = "http://192.168.0.51:9005/secondB/common_board_list.foc?com_b_type=2";
+                        break;
+                    case 4:  //봉사게시판
+                        url = "http://192.168.0.51:9005/secondB/common_board_list.foc?com_b_type=1";
+                        break;
+                }
+
+                //fragment 생성
+                BoardFragment boardFragment = new BoardFragment();
+                //가운데 레이아웃 웹뷰로 바꿔치기
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.relLayout2_middle, boardFragment)
+                        //뒤로가기에 필요
+                        .addToBackStack("boardList")
+                        .commit();
+                //번들객체 생성, text값 저장
+                Bundle bundle = new Bundle();
+                bundle.putString("url",url);
+                bundle.putInt("caseNum", caseNum);
+                // 번들 전달
+                boardFragment.setArguments(bundle);
+            }
+        }) ;
+
         setupBottomNavigationView();
     }
+
+
 
     //하단바 설정
     private void setupBottomNavigationView(){
---
+++ app/src/main/java/com/example/forcatapp/Board/BoardFragment.java
@@ -0,0 +1,146 @@
+package com.example.forcatapp.Board;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.example.forcatapp.R;
+
+import kr.co.bootpay.BootpayWebView;
+import kr.co.bootpay.listener.EventListener;
+
+public class BoardFragment extends Fragment {
+    private static final String TAG = "BoardFragment";
+    BootpayWebView wv_web;
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_boardlist, container, false);
+
+        //이전버튼 이벤트 처리
+        ImageView backArrow = view.findViewById(R.id.common_back);
+        backArrow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: 게시판창 뒤로가기");
+                getActivity().onBackPressed();
+            }
+        });
+
+        wv_web = view.findViewById(R.id.wv_board_boardlist);
+        WebSettings webSettings = wv_web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        //CSS 깨짐 방지-----------------------------
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+
+        //CSS 깨짐 방지-----------------------------
+        Intent intent = getActivity().getIntent(); /*데이터 수신*/
+        //BoardActivity에서 전달한 번들 저장
+        Bundle bundle = getArguments();
+        //번들 안의 텍스트 불러오기
+        String url = bundle.getString("url");
+        int caseNum = bundle.getInt("caseNum");
+        if(caseNum == 1){
+            bootpay(wv_web, url);
+        }
+
+        wv_web.loadUrl(url);
+        wv_web.setWebViewClient(new WebViewClient());
+
+        //아래로 스와이프해서 새로고침하기
+        SwipeRefreshLayout refreshLayout = view.findViewById(R.id.swipe_refresh);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                wv_web.reload();
+            }
+        });
+
+        wv_web.setWebViewClient(new WebViewClient()
+        {
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                refreshLayout.setRefreshing(false);
+            }
+
+        });
+
+        return view;
+    }
+
+    private void bootpay(BootpayWebView wv_web, String url) {
+        wv_web.setOnResponseListener(new EventListener() {
+            @Override
+            public void onError(String data) {
+                Log.d(TAG, "onError: " + data);
+            }
+
+            @Override
+            public void onCancel(String data) {
+                Log.d(TAG, "onCancel: " + data);
+            }
+
+            @Override
+            public void onClose(String data) {
+                Log.d(TAG, "onClose: " + data);
+            }
+
+            @Override
+            public void onReady(String data) {
+                Log.d(TAG, "onReady: " + data);
+            }
+
+            @Override
+            public void onConfirm(String data) {
+                boolean iWantPay = true;
+                if(iWantPay == true) { // 재고가 있을 경우
+                    doJavascript("BootPay.transactionConfirm( " + data + ");");
+                } else {
+                    doJavascript("BootPay.removePaymentWindow();");
+                }
+            }
+            //===========================================[[ 결제 완료 ]]=================================================
+            @Override
+            public void onDone(String data) {
+                Log.d(TAG, "onDone: " + data);
+
+            }
+        });
+        wv_web.loadUrl(url);
+        }
+
+    public void doJavascript(String script) {
+        final String str = script;
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                wv_web.loadUrl("javascript:(function(){" + str + "})()");
+            }
+        });
+    }
+
+    class MyViewClient extends WebViewClient {
+        //웹뷰 내에서 웹 페이지를 불러올때 사용함.
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            super.shouldOverrideUrlLoading(view,url);
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+}
